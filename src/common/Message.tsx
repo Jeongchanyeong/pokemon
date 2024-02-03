9fa06565916372e52f44e89bc74219ba992de537
정찬영 <q135794268020@gmail.com>
2024-02-04 07:52:24 +0900

    Feat/ assets 폴더 png파일 삽입

--- src/common/Message.tsx
+++ src/common/Message.tsx
@@ -14,8 +14,6 @@ const Text = styled.p`
   overflow: hidden;
   animation: ${typingAnimation} 1s steps(30) 1;
   white-space: nowrap;
-
-  padding-left: 40px;
 `;
 
 const Message = ({ text }: any) => {
