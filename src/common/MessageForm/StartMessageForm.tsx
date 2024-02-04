7a81ef67740f1526305a93719d59a3c198734fdc
정찬영 <q135794268020@gmail.com>
2024-02-05 04:06:53 +0900

    Feat/ StartPage 구현 완료 (부수적인 디자인 및 반응형 추후 수정 예정)

--- src/common/MessageForm/StartMessageForm.tsx
+++ src/common/MessageForm/StartMessageForm.tsx
@@ -4,48 +4,53 @@ import Message from '../Message';
 import GlobalButton from '../GlobalButton';
 import { START_MESSAGE } from '../../constants/message';
 
-const StartMessageForm = ({ showButton }: any) => {
+const Wrapper = styled.div`
+  display: flex;
+  background-color: #242424;
+  border: 5px ridge white;
+  border-radius: 15px;
+  flex-direction: column;
+
+  width: 1000px;
+  height: 200px;
+  max-width: 100%;
+  margin: 0 auto;
+`;
+
+const MessageFormWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding-top: 45px;
+  font-size: 18px;
+  flex: 4;
+  padding-left: 40px;
+  padding-right: 40px;
+  color: white;
+`;
+
+const GlobalButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  text-align: center;
+  margin: 0 20px 20px 0;
+
+  button {
+    color: white;
+    background-color: #606060;
+    font-size: 12px;
+  }
+`;
+interface StartMessageFormProps {
+  changeButtonForm: () => void;
+}
+
+const StartMessageForm: React.FC<StartMessageFormProps> = ({
+  changeButtonForm,
+}) => {
   const [messageIndex, setMessageIndex] = useState<number>(0);
   const [visibleForm, setVisibleForm] = useState(true);
-
-  const Wrapper = styled.div`
-    display: flex;
-    background-color: #242424;
-    border: 5px ridge white;
-    border-radius: 15px;
-    flex-direction: column;
-
-    width: 1000px;
-    height: 200px;
-    max-width: 100%;
-    margin: 0 auto;
-  `;
-
-  const MessageFormWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    padding-top: 45px;
-    font-size: 18px;
-    flex: 4;
-    padding-left: 40px;
-    padding-right: 40px;
-    color: white;
-  `;
-
-  const GlobalButtonWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    text-align: center;
-    margin: 0 20px 20px 0;
-
-    button {
-      color: white;
-      background-color: #606060;
-      font-size: 12px;
-    }
-  `;
 
   const nextIndex = (idx: number) => (idx + 1) % START_MESSAGE.length;
 
@@ -55,20 +60,11 @@ const StartMessageForm = ({ showButton }: any) => {
   };
 
   useEffect(() => {
-    if (messageIndex === START_MESSAGE.length - 1) {
+    if (messageIndex === 4) {
       setVisibleForm(false);
+      changeButtonForm();
     }
-
-    return () => {
-      showButton(true);
-    };
-  }, [showButton]);
-
-  // setVisibleForm(false)가 되지 않고 showButton(true)가 됨
-  // 그 후 > 버튼을 누르다 보면 setVisibleForm(false)가 됨
-
-  // 첫 렌더링 시에 적용이 안 되는 것인가?
-  // 첫 렌더링 시 적용인 된다만  setVisibleForm(false)가 적용되지 않는다
+  }, [messageIndex]);
 
   return (
     <>
--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,80 +1,81 @@
 import styled from 'styled-components';
 import StartMessageForm from '../common/MessageForm/StartMessageForm';
-import { useState, useEffect } from 'react';
 import GlobalButton from '../common/GlobalButton';
 import { useNavigate } from 'react-router-dom';
 import { keyframes } from 'styled-components';
+import { useState } from 'react';
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #080808;
+  justify-content: center;
+`;
+
+const MainContentWrapper = styled.div`
+  width: 80vw;
+  height: 100vh;
+  background-color: #1a1a1a;
+  background-image: url('../assets/backgroundCity.png');
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  flex-direction: column;
+`;
+
+const shadowAnimation = keyframes`  
+from {
+box-shadow: 42px 42px 42px #dbdbdb, -42px -42px 42px #dbdbdb,
+            -42px 42px 42px #dbdbdb, 42px -42px 42px #dbdbdb;
+}
+to {
+box-shadow: 50px 50px 75px #dbdbdb, -50px -50px 75px #dbdbdb,
+            -50px 50px 75px #dbdbdb, 50px -50px 75px #dbdbdb;
+}
+`;
+
+const DoctorOh = styled.div`
+  background-color: #dbdbdb;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${shadowAnimation} 1.2s infinite alternate;
+
+  flex: 7;
+  margin-top: 180px;
+  img {
+    width: 80%;
+    max-width: 460px;
+    height: auto;
+  }
+`;
+
+const StartMessageFormWrapper = styled.div`
+  flex: 4;
+`;
 
 const Start = () => {
-  const [buttonVisible, setButtonVisible] = useState<boolean>(false);
-
+  const [visibleButton, setVisibleButton] = useState(false);
   const navigate = useNavigate();
-
-  const Wrapper = styled.div`
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    background-color: #080808;
-    justify-content: center;
-  `;
-
-  const MainContentWrapper = styled.div`
-    width: 80vw;
-    height: 100vh;
-    background-color: #1a1a1a;
-    background-image: url('../assets/backgroundCity.png');
-    background-size: cover;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    flex-direction: column;
-  `;
-
-  const shadowAnimation = keyframes`  
-  from {
-    box-shadow: 42px 42px 42px #dbdbdb, -42px -42px 42px #dbdbdb,
-                -42px 42px 42px #dbdbdb, 42px -42px 42px #dbdbdb;
-  }
-  to {
-    box-shadow: 50px 50px 75px #dbdbdb, -50px -50px 75px #dbdbdb,
-                -50px 50px 75px #dbdbdb, 50px -50px 75px #dbdbdb;
-  }
-  `;
-
-  const DoctorOh = styled.div`
-    background-color: #dbdbdb;
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    animation: ${shadowAnimation} 1.2s infinite alternate;
-
-    flex: 7;
-    margin-top: 180px;
-    img {
-      width: 80%;
-      max-width: 460px;
-      height: auto;
-    }
-  `;
-
-  const StartMessageFormWrapper = styled.div`
-    flex: 4;
-  `;
-  const showButton = (isVisible: boolean) => {
-    setButtonVisible(isVisible);
-  };
 
   const handleClick = () => {
     navigate('/list');
   };
 
-  // 폼이 사라진 뒤 버튼이 생성됨을 관리
-  // buttonVisible로 버튼의 boolean값 관리
-  // prop으로 뭐를 내려주고 startMessageForm에서 그걸 prop으로 받아 Form이 없어지면 buttonVisible로(true)로 변경해주면 되겠다.
+  // 해당 컴포넌트에서는 해당 컴포넌트 내의 엘리먼트의 상태를 관리해준다.
+  // 만약 자식 form이 삭제될 때 a의 상태를 관리하고 싶다면
+  // 여기서 작성한 함수 const aDissapear = ()=>{a(false)} 얘를 자식 컴포넌트에 전달
+  // 자식 컴포넌트에서는 이러한 조건에서 애가 사라질 때 해당 힘수를 호출만 해준다.
+
+  const changeButtonForm = () => {
+    setVisibleButton(true);
+  };
 
   return (
     <Wrapper>
@@ -82,10 +83,11 @@ const Start = () => {
         <DoctorOh>
           <img src="../assets/dr.oh.png" />
         </DoctorOh>
+
         <StartMessageFormWrapper>
-          <StartMessageForm showButton={showButton} />
-          {/* wrapper안에 메세지 폼과 버튼이 둘다 있지만 버튼은 true일 때만 떠 */}
-          {buttonVisible && (
+          <StartMessageForm changeButtonForm={changeButtonForm} />
+          {/* 버튼은 true일 때만 생성됨.  */}
+          {visibleButton && (
             <GlobalButton onClick={handleClick} buttontype="start">
               {'START'}
             </GlobalButton>
