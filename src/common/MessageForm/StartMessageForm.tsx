cacf8b25d027d38e3e0b93c0daf35ae8ffaa30b0
정찬영 <q135794268020@gmail.com>
2024-02-04 23:46:38 +0900

    Feat/ StartPage 구현 (기능 구현 진행중)

--- src/common/MessageForm/StartMessageForm.tsx
+++ src/common/MessageForm/StartMessageForm.tsx
@@ -1,54 +1,91 @@
-import styled from 'styled-components';
-import { useState } from 'react';
+import { styled } from 'styled-components';
+import { useEffect, useState } from 'react';
 import Message from '../Message';
 import GlobalButton from '../GlobalButton';
+import { START_MESSAGE } from '../../constants/message';
 
-const StartMessageForm = () => {
+const StartMessageForm = ({ showButton }: any) => {
   const [messageIndex, setMessageIndex] = useState<number>(0);
-
-  const startMessage: string[] = [
-    '왜 이렇게 늦은거야 자네..! 포켓몬들이 얼마나 기다리고 있었는지 알아?',
-    '늦은 감이 있긴 하지만... 포켓몬 세계에 다시 돌아온 것을 환영하네.',
-    '잊지 않았지? 여기서는 원하는 포켓몬들 잡을 수도, 별명을 지어줄 수도, 원하지 않는다면 방출 시킬수도 있지.',
-    '뭐 예전에 다 해본 것들이니 금방 할 수 있을거라고 믿는다네. 일단 어서 들어와!!',
-  ];
+  const [visibleForm, setVisibleForm] = useState(true);
 
   const Wrapper = styled.div`
     display: flex;
-    background-color: #b9b9b9;
+    background-color: #242424;
+    border: 5px ridge white;
+    border-radius: 15px;
     flex-direction: column;
+
     width: 1000px;
     height: 200px;
+    max-width: 100%;
+    margin: 0 auto;
   `;
 
   const MessageFormWrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: flex-start;
-    padding-top: 25px;
+    padding-top: 45px;
+    font-size: 18px;
     flex: 4;
+    padding-left: 40px;
+    padding-right: 40px;
+    color: white;
   `;
 
   const GlobalButtonWrapper = styled.div`
     display: flex;
+    align-items: center;
     justify-content: flex-end;
-    flex: 1;
+    text-align: center;
+    margin: 0 20px 20px 0;
+
+    button {
+      color: white;
+      background-color: #606060;
+      font-size: 12px;
+    }
   `;
 
+  const nextIndex = (idx: number) => (idx + 1) % START_MESSAGE.length;
+
+  // 클릭시 다음 배열의 인덱스 보여주기
   const handleClick = () => {
-    setMessageIndex((idx: number) => (idx + 1) % startMessage.length);
+    setMessageIndex(nextIndex);
   };
 
-  return (
-    <Wrapper>
-      <MessageFormWrapper>
-        <Message text={startMessage[messageIndex]} />
-      </MessageFormWrapper>
+  useEffect(() => {
+    if (messageIndex === START_MESSAGE.length - 1) {
+      setVisibleForm(false);
+    }
 
-      <GlobalButtonWrapper>
-        <GlobalButton onClick={handleClick} text={'>'} />
-      </GlobalButtonWrapper>
-    </Wrapper>
+    return () => {
+      showButton(true);
+    };
+  }, [showButton]);
+
+  // setVisibleForm(false)가 되지 않고 showButton(true)가 됨
+  // 그 후 > 버튼을 누르다 보면 setVisibleForm(false)가 됨
+
+  // 첫 렌더링 시에 적용이 안 되는 것인가?
+  // 첫 렌더링 시 적용인 된다만  setVisibleForm(false)가 적용되지 않는다
+
+  return (
+    <>
+      {visibleForm && (
+        <Wrapper>
+          <MessageFormWrapper>
+            <Message text={START_MESSAGE[messageIndex]} />
+          </MessageFormWrapper>
+
+          <GlobalButtonWrapper>
+            <GlobalButton buttontype="small" onClick={handleClick}>
+              {'>'}
+            </GlobalButton>
+          </GlobalButtonWrapper>
+        </Wrapper>
+      )}
+    </>
   );
 };
 
--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,44 +1,97 @@
 import styled from 'styled-components';
 import StartMessageForm from '../common/MessageForm/StartMessageForm';
+import { useState, useEffect } from 'react';
+import GlobalButton from '../common/GlobalButton';
+import { useNavigate } from 'react-router-dom';
+import { keyframes } from 'styled-components';
 
-// start 페이지는 해당 폼이 등장, 사라짐만 관리를 할까?
 const Start = () => {
+  const [buttonVisible, setButtonVisible] = useState<boolean>(false);
+
+  const navigate = useNavigate();
+
   const Wrapper = styled.div`
     width: 100vw;
     height: 100vh;
-    background-color: #1f1f1f;
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    h1 {
-      color: white;
+    background-color: #080808;
+    justify-content: center;
+  `;
+
+  const MainContentWrapper = styled.div`
+    width: 80vw;
+    height: 100vh;
+    background-color: #1a1a1a;
+    background-image: url('../assets/backgroundCity.png');
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    flex-direction: column;
+  `;
+
+  const shadowAnimation = keyframes`  
+  from {
+    box-shadow: 42px 42px 42px #dbdbdb, -42px -42px 42px #dbdbdb,
+                -42px 42px 42px #dbdbdb, 42px -42px 42px #dbdbdb;
+  }
+  to {
+    box-shadow: 50px 50px 75px #dbdbdb, -50px -50px 75px #dbdbdb,
+                -50px 50px 75px #dbdbdb, 50px -50px 75px #dbdbdb;
+  }
+  `;
+
+  const DoctorOh = styled.div`
+    background-color: #dbdbdb;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    animation: ${shadowAnimation} 1.2s infinite alternate;
+
+    flex: 7;
+    margin-top: 180px;
+    img {
+      width: 80%;
+      max-width: 460px;
+      height: auto;
     }
   `;
 
-  const Column = styled.div`
-    background-color: black;
-    width: 300px;
-    height: 100vh;
-    display: flex;
+  const StartMessageFormWrapper = styled.div`
+    flex: 4;
   `;
+  const showButton = (isVisible: boolean) => {
+    setButtonVisible(isVisible);
+  };
 
-  // const StartButton = styled.button`
-  //   background-color: #851729;
-  //   color: #ffffff;
-  //   font-size: 150px;
-  //   font-family: 'Galmuri11', sans-serif;
+  const handleClick = () => {
+    navigate('/list');
+  };
 
-  //   border-radius: 20px;
-  //   border: 4px dotted;
-  //   border-color: #fff88b;
-  // `;
+  // 폼이 사라진 뒤 버튼이 생성됨을 관리
+  // buttonVisible로 버튼의 boolean값 관리
+  // prop으로 뭐를 내려주고 startMessageForm에서 그걸 prop으로 받아 Form이 없어지면 buttonVisible로(true)로 변경해주면 되겠다.
 
   return (
     <Wrapper>
-      <Column></Column>
-      <StartMessageForm />
-      {/* 메세지 폼이 사라질 때 StartButton이 뜨도록 구현 */}
-      <Column></Column>
+      <MainContentWrapper>
+        <DoctorOh>
+          <img src="../assets/dr.oh.png" />
+        </DoctorOh>
+        <StartMessageFormWrapper>
+          <StartMessageForm showButton={showButton} />
+          {/* wrapper안에 메세지 폼과 버튼이 둘다 있지만 버튼은 true일 때만 떠 */}
+          {buttonVisible && (
+            <GlobalButton onClick={handleClick} buttontype="start">
+              {'START'}
+            </GlobalButton>
+          )}
+        </StartMessageFormWrapper>
+      </MainContentWrapper>
     </Wrapper>
   );
 };
