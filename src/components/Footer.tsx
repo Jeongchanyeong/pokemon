896c3f858900c69e7e31d762ddb72e05f149d984
정찬영 <q135794268020@gmail.com>
2024-02-14 20:59:42 +0900

    Feat/ Footer 제작

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  background-color: blue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: #b4cbbe;
   width: 100%;
-  height: 50px;
+  height: 5%;
+  padding: 40px;
+  color: #ffffff;
+  span {
+    padding: 20px;
+  }
 `;
 const Footer = () => {
-  return <Wrapper>이곳은 footer입니다</Wrapper>;
+  return (
+    <Wrapper>
+      <span>
+        본 페이지는 상업적 목적이 아닌 개인 포트폴리오용으로 제작되었습니다.
+      </span>
+      <span>© 2023 Jeong, Chan-Yeong. All Rights Reserved.</span>
+    </Wrapper>
+  );
 };
 
 export default Footer;
